671c7f9e489cf17e5c5f48b9d59624775f10bad5
hashtagp <hashtag.p27@gmail.com>
2025-02-16 14:47:36 +0530

    update database configuration to use environment variable for MongoDB URI

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectToDatabase = async () => {
   try {
-    const connection = await mongoose.connect('your-mongodb-uri', {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
